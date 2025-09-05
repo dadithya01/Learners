37a6db7cd6142ff4e72d6d4477b753f81fa088bd
dadithya01 <dadithya01@gmail.com>
2025-09-05 17:44:46 +0530

    login,main page ui changes and admin dashboard ui and hibernate.properties

--- src/main/java/edu/ijse/learners/controllers/LoginPageController.java
+++ src/main/java/edu/ijse/learners/controllers/LoginPageController.java
@@ -1,4 +1,21 @@
 package edu.ijse.learners.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class LoginPageController {
+
+    @FXML
+    private AnchorPane ancMain;
+
+    @FXML
+    void login(ActionEvent event) throws IOException {
+        ancMain.getChildren().clear();
+        AnchorPane load = FXMLLoader.load(getClass().getResource("/view/AdminDashboard.fxml"));
+        ancMain.getChildren().add(load);
+    }
 }
