927854e028229d01226106014f705c0400eed032
dadithya01 <dadithya01@gmail.com>
2025-09-07 13:41:25 +0530

    Enhance Admin Dashboard UI with management buttons and update font styles across multiple FXML files

--- src/main/java/edu/ijse/learners/controllers/AdminDashboardController.java
+++ src/main/java/edu/ijse/learners/controllers/AdminDashboardController.java
@@ -1,4 +1,48 @@
 package edu.ijse.learners.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class AdminDashboardController {
+
+    @FXML
+    void courseManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void instructorManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lessonManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(MouseEvent event) {
+
+    }
+
+    @FXML
+    void paymentManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void settings(MouseEvent event) {
+
+    }
+
+    @FXML
+    void studentManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userManagement(ActionEvent event) {
+
+    }
 }
