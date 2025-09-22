9760b558002883ca809dd1961a81c70534d421a5
adithya <dadithya01@gmail.com>
2025-09-22 12:22:03 +0530

    update pom and module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,18 @@
 module edu.ijse.learners {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.hibernate.orm.core;
     requires static lombok;
     requires jakarta.persistence;
-    requires org.hibernate.orm.core;
-    exports edu.ijse.learners.controllers;
     requires java.naming;
-    requires java.sql;
+    requires jbcrypt;
 
+
+    opens edu.ijse.learners.configuration to jakarta.persistence;
+    opens edu.ijse.learners.entity to org.hibernate.orm.core;
 
     opens edu.ijse.learners.controllers to javafx.fxml;
+    opens edu.ijse.learners.tdm to javafx.base;
     opens edu.ijse.learners to javafx.fxml;
     exports edu.ijse.learners;
 }
